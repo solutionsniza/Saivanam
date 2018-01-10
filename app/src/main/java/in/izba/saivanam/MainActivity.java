d340aa14e3b67b2d561196a5dec2b4d2520f0624
solutionsniza <35291598+solutionsniza@users.noreply.github.com>
2018-01-10 18:35:17 +0530

    Update MainActivity.java

--- app/src/main/java/in/izba/saivanam/MainActivity.java
+++ app/src/main/java/in/izba/saivanam/MainActivity.java
@@ -9,11 +9,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        //Saivanam project application with google maps
     }
 
     public void add(){
         int i = 1+1;
+        int j = 2+2;
     }
 }
